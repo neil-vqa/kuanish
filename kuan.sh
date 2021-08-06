078616cbad8f5927c94efd9198646c4bee890000
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2021-08-06 10:02:20 +0800

    redirect jq errors to /dev/null

--- kuan.sh
+++ kuan.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/bash
 
 get_synonym() {
-        curl -s https://www.dictionaryapi.com/api/v3/references/thesaurus/json/$1?key=$MW_THESAURUS | jq .[].meta.syns
+        curl -s https://www.dictionaryapi.com/api/v3/references/thesaurus/json/$1?key=$MW_THESAURUS | jq .[].meta.syns 2> /dev/null || echo "No synonyms available."
 }
 
 get_definition() {
-        curl -s https://www.dictionaryapi.com/api/v3/references/collegiate/json/$1?key=$MW_DICTIONARY | jq .[].shortdef
+        curl -s https://www.dictionaryapi.com/api/v3/references/collegiate/json/$1?key=$MW_DICTIONARY | jq .[].shortdef 2> /dev/null || echo "No definitions available."
 }
 
 echo "Other words for $1" ; echo "$(get_synonym $1)"
