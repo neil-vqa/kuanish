7ed13ac82b0fba642f27abe254780187531470d4
Neil Van <49968200+neil-vqa@users.noreply.github.com>
2021-08-26 23:31:19 +0800

    format response object
    create object from response with the keys: definition, thumbs_up, and example

--- kuanba.sh
+++ kuanba.sh
@@ -1,1 +1,5 @@
-printf "$(curl -s http://api.urbandictionary.com/v0/define?term=$1 | jq ".list[]" 2> /dev/null || echo "Definitions not available.")"
+#!/usr/bin/bash
+
+printf "$(curl -s https://api.urbandictionary.com/v0/define?term=$1 | \
+jq '[.list[] | {"definition": .definition, "thumbs_up": .thumbs_up, "example": .example}] | sort_by(-.thumbs_up)' || \
+echo 'Definitions not available.')" | less
